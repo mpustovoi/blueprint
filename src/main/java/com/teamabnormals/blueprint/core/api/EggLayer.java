b8735aa6b61affead55e9623170501080f33e258
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-04-21 13:25:11 -0500

    Added EggLayer interface for easier integration

---
+++ src/main/java/com/teamabnormals/blueprint/core/api/EggLayer.java
@@ -0,0 +1,19 @@
+package com.teamabnormals.blueprint.core.api;
+
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.item.Item;
+
+public interface EggLayer {
+	int getEggTimer();
+
+	void setEggTimer(int time);
+
+	boolean isBirdJockey();
+
+	Item getEggItem();
+
+	int getNextEggTime(RandomSource rand);
+
+	SoundEvent getEggLayingSound();
+}
