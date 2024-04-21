d8198efc9525b916207a39556fb7aaaba8d427db
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2024-04-21 14:19:57 -0500

    Implement EggLayer into Chicken

---
+++ src/main/java/com/teamabnormals/blueprint/core/mixin/ChickenMixin.java
@@ -0,0 +1,58 @@
+package com.teamabnormals.blueprint.core.mixin;
+
+import com.teamabnormals.blueprint.core.api.EggLayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+@Mixin(Chicken.class)
+public abstract class ChickenMixin extends Animal implements EggLayer {
+
+	protected ChickenMixin(EntityType<? extends Animal> type, Level level) {
+		super(type, level);
+	}
+
+	@Shadow
+	public int eggTime;
+
+	@Shadow
+	public boolean isChickenJockey;
+
+	@Override
+	public int getEggTimer() {
+		return this.eggTime;
+	}
+
+	@Override
+	public void setEggTimer(int time) {
+		this.eggTime = time;
+	}
+
+	@Override
+	public boolean isBirdJockey() {
+		return this.isChickenJockey;
+	}
+
+	@Override
+	public Item getEggItem() {
+		return Items.EGG;
+	}
+
+	@Override
+	public int getNextEggTime(RandomSource rand) {
+		return rand.nextInt(6000) + 6000;
+	}
+
+	@Override
+	public SoundEvent getEggLayingSound() {
+		return SoundEvents.CHICKEN_EGG;
+	}
+}
